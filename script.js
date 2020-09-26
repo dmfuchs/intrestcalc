b8032a9ccdd042358e1cb6e6ba4efdfd377d2c09
dmfuchs <dm.fuchs00@gmail.com>
2020-09-26 22:44:34 +0200

    Final

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("intrestCalc").onclick = function (){
+
+   let num1 = document.getElementById("randAmount").value;
+   let num2 = document.getElementById("intField").value;
+
+    let i = +num1 + +num1*((+num2/100))
+
+    let intrest = i.toFixed(2)
+
+
+ document.getElementById("calcAmount").innerHTML = 'R' + intrest;
+}
